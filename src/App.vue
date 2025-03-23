dde8da783b2874d998ea688c2b41f68efe48e9c4
Hacker God <jyrone.parker@gmail.com>
2025-03-23 12:25:18 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <TerrainViewer />
+</template>
+
+<script setup lang="ts">
+import TerrainViewer from './components/TerrainViewer.vue'
+</script>
+
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+}
+</style>
+
---
+++ src/components/TerrainViewer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <TerrainGenerator 
+      :terrainSize="1500"
+      :segments="4096"
+      :heightScaling="100"
+      noiseType="simplex"
+      :noiseScale="0.04"
+      terrainColor="#000"
+      backgroundColor="#FFF"
+     
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TerrainGenerator from '@mastashake08/terrain-generator'
+</script>
+
+<style scoped>
+div {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
+
